4b232d53b97d3080551e688f72711855d1641b37
kbuczek <krzysztofbuczek@live.com>
2024-12-17 00:50:43 +0100

    coin script update

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,12 +1,5 @@
-extends AnimatedSprite2D
+extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("coin!");
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin!");
+	queue_free();
